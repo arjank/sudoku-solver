3ce4f51388b061f91841ee895efc1d4ec7b14ec6
Arjan <github@arjanonline.net>
2017-03-05 22:42:52 +0100

    Fixes broken test

--- src/test/scala-2.12/net/arjanonline/sudoku_solver/SolverSuite.scala
+++ src/test/scala-2.12/net/arjanonline/sudoku_solver/SolverSuite.scala
@@ -36,6 +36,17 @@ class SolverSuite extends FunSuite {
       |4 6 1 3 2 5
       |3 5 2 1 6 4""".stripMargin
 
+  val solution9x9: String =
+    """4 6 8 7 1 9 2 3 5
+      |2 3 7 4 8 5 6 1 9
+      |1 5 9 3 6 2 7 8 4
+      |3 1 2 6 5 7 9 4 8
+      |6 7 4 1 9 8 5 2 3
+      |9 8 5 2 3 4 1 7 6
+      |7 2 6 9 4 3 8 5 1
+      |8 9 3 5 2 1 4 6 7
+      |5 4 1 8 7 6 3 9 2""".stripMargin
+
   println(solution6x6)
 
   test("Solver should find correct solution for 6x6 sudoku") {
@@ -44,15 +55,15 @@ class SolverSuite extends FunSuite {
     }
   }
 
-//  test("Solver should find only one solution") {
-//    new Board6x6 {
-//      assert(completeSolutions.length === 1)
-//    }
-//  }
+  test("Solver should find only one solution") {
+    new Board6x6 {
+      assert(completeSolutions.length === 1)
+    }
+  }
 
   test("Solver should find correct solution for 9x9 sudoku") {
     new Board9x9 {
-      assert(solution.get.toString === solution6x6)
+      assert(solution.get.toString === solution9x9)
     }
   }
 }
